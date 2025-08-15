d246e4e6bbed13eba89d8ee1943683fcfa038506
pramesh gicl <gicl.pramesh@gmail.com>
2025-08-15 18:02:48 +0000

    failed 2 minutes ago in 49s 4s 0s 0s 3s 1s 0s 19s 19s Run npx --no-insta

--- src/app/projects/[slug]/page.tsx
+++
@@ -1,6 +0,0 @@
-// This file has been temporarily removed to resolve a persistent build error.
-// The project detail pages will be restored in a future update.
-
-export default function ProjectPage() {
-  return null;
-}
